e41cc5f78641a5f530e8600ce3d0c374c8d369ec
Ahmet Alperen Yılmazyıldız <46990789+AlperenYilmazyildiz@users.noreply.github.com>
2025-03-03 20:05:10 +0300

    tests are finalized

--- src/tests/loginTests.js
+++ src/tests/loginTests.js
@@ -1,4 +1,4 @@
-const { Builder, By, Key, until, Actions } = require("selenium-webdriver");
+const { Builder, By, Key, until } = require("selenium-webdriver");
 
 async function testInvalidLogin() {
     let driver = await new Builder().forBrowser("firefox").build();
@@ -17,9 +17,8 @@ async function testInvalidLogin() {
         await driver.wait(until.elementLocated(By.id('error-message')), 10000);
         
         let errorMessage = await driver.findElement(By.id('error-message')).getText();
-        console.log("error message", errorMessage);
         if (errorMessage.includes("Login failed. Wrong email or password.")) {
-            console.log("Test Passed: Invalid Login Handled Correctly");
+            console.log("✅Test Passed: Invalid Login Handled Correctly");
         } else {
             console.log("Test Failed: Unexpected error message - " + errorMessage);
         }
@@ -48,9 +47,8 @@ async function testWrongEmailFormat() {
         await driver.wait(until.elementLocated(By.id('error-message')), 10000);
         
         let errorMessage = await driver.findElement(By.id('error-message')).getText();
-        console.log("error message", errorMessage);
         if (errorMessage.includes("Email format is incorrect. Email must contain one '@', characters before '@' and after '@'.")) {
-            console.log("Test Passed: Invalid Email Handled Correctly");
+            console.log("✅Test Passed: Invalid Email Handled Correctly");
         } else {
             console.log("Test Failed: Unexpected error message - " + errorMessage);
         }
@@ -78,7 +76,7 @@ async function testMultipleFailedAttempts() {
         let text = await loginButton.getText();
 
         if (text.includes("Try again in")){
-            console.log("Test Passed: Handled Multiple Failed Attempts Correctly");
+            console.log("✅Test Passed: Handled Multiple Failed Attempts Correctly");
         } else {
             console.log("Test Failed: Unexpected behaviour");
         }
@@ -147,27 +145,24 @@ async function testLoginWithSameEmail() {
         let emailField = await driver.wait(until.elementLocated(By.xpath('//input[@type="email"]')), 3000).sendKeys("aayytest123@gmail.com");
         let nextButton = await driver.wait(until.elementLocated(By.xpath('//span[text()="Next"]'), 3000)).click();
         driver.sleep(2000);
-        // await driver.wait(until.elementLocated(By.xpath('//input[@type="checkbox"]'), 3000));
-        // let element =  driver.findElement(By.xpath('//input[@type="checkbox"]'));
-        // driver.executeScript("arguments[0].click();", element);
-        // await driver.wait(until.elementLocated(By.xpath('//input[@type="text"]')), 3000);
-        // let passwordField = driver.findElement(By.xpath('//input[@type="text"]'));
-        let element = await driver.findElement(By.css('input[name="hiddenPassword"]'));
-        let overlappingElement = await driver.findElement(By.css('.overlapping-element')); // Adjust the selector
-        await driver.executeScript("arguments[0].style.display = 'none';", overlappingElement);
-        await element.sendKeys("test1234?");
-        //await passwordField.sendKeys("test1234?");
+        await driver.wait(until.elementLocated(By.xpath('//input[@type="checkbox"]'), 3000));
+        let element =  driver.findElement(By.xpath('//input[@type="checkbox"]'));
+        driver.executeScript("arguments[0].click();", element);
+        await driver.wait(until.elementLocated(By.xpath('//input[@type="text"]')), 3000);
+        let passwordField = driver.findElement(By.xpath('//input[@type="text"]'));
+        // let element = await driver.findElement(By.css('input[name="hiddenPassword"]'));
+        // let overlappingElement = await driver.findElement(By.css('.overlapping-element')); // Adjust the selector
+        // await driver.executeScript("arguments[0].style.display = 'none';", overlappingElement);
+        // await element.sendKeys("test1234?");
+        await passwordField.sendKeys("test1234?");
         let secondNextButton = await driver.wait(until.elementLocated(By.xpath('//span[text()="Next"]'), 3000)).click();
     
-        // let alert = await driver.switchTo().alert();
-        // let alertText = await alert.getText(); 
+        let alert = await driver.switchTo().alert();
+        let alertText = await alert.getText(); 
 
-        // if (alertText.includes("User with email aayytest123@gmail.com already exists.")) {
-        //     console.log("✅Test Passed: Google OAuth Test Passed: User already exists");
-        // }
-        // else {
-        //     console.log("Test Failed: Unexpected alert text - " + alertText);
-        // }   
+        if (alertText.includes("User with email aayytest123@gmail.com already exists.")) {
+            console.log("✅Test Passed: User with same email already exists warning displayed");
+        }  
     } catch (error) {   
         console.error("Test Failed:", error.message);
     }
@@ -176,8 +171,8 @@ async function testLoginWithSameEmail() {
     }
 }
 
-//testInvalidLogin();
-//testWrongEmailFormat();
-//testMultipleFailedAttempts();
-//testGoogleLogin();
+testInvalidLogin();
+testWrongEmailFormat();
+testMultipleFailedAttempts();
+testGoogleLogin();
 testLoginWithSameEmail();
